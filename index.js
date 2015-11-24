37fd840a7533f1a4a5c02f8b6baa4315938c672f
Tim Williams <tim.williams@baesystems.com>
2015-11-24 17:48:44 +0000

    Logic to search for data based on collection name

--- index.js
+++ index.js
@@ -1,14 +1,48 @@
 #!/usr/bin/env node
+var path = require('path');
 var program = require('commander');
+var fs = require('fs');
+var newman = require('newman');
 
 program.arguments('<file> [otherFiles...]')
 .option('-e, --environment <environment>', 'The postman environment to use')
+.option('-d, --data-path <dataPath>', 'The directory to search for data files')
 .action(function(file, otherFiles) {
-	console.log('environment: %s file: %s', program.environment, file);
+	console.log('Starting newman run for environment: %s', program.environment);
+	console.log('Will search datapath: %s', program.dataPath)
+	console.log(file);
+	
+	var allFiles = [ file ];
 	if (otherFiles)	{
-		otherFiles.forEach(function(element) {
-			console.log(element);
+		otherFiles.forEach(function(otherFile) {
+			console.log(otherFile);
 		});
+		allFiles.concat(otherFiles);
 	}
+	
+	runPostmanForFiles(allFiles, program.dataPath, program.environment);
 })
 .parse(process.argv);
+
+function runPostmanForFiles(allFiles, dataPath, environment) {
+	allFiles.forEach(function(file) {
+		
+		var split = path.basename(file).split('.');
+		console.log('Looking for data for %s', split[0]);
+		
+		var dataFile = path.join(dataPath, split[0] + ".data.json");
+	    console.log('Checking %s', dataFile);
+		
+		if (!fs.statSync(dataFile).isFile()) {
+			console.log('File does not exist');
+			process.exit(1);
+     	}
+		
+		runPostman(file, dataFile, environment);	
+	})
+}
+
+function runPostman(file, dataFile, environment) {
+	
+	
+}
