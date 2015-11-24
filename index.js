8a3b96b553b16863dc495619f812109f2c0b3728
Tim Williams <tim.williams@baesystems.com>
2015-11-24 15:34:34 +0000

    Add Visual Studio Code support

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 #!/usr/bin/env node
-console.log('Hello, world!');
+var program = require('commander');
+
+program.arguments('<file> [otherFiles...]')
+.option('-e, --environment <environment>', 'The postman environment to use')
+.action(function(file, otherFiles) {
+	console.log('environment: %s file: %s', program.environment, file);
+	if (otherFiles)	{
+		otherFiles.forEach(function(element) {
+			console.log(element);
+		});
+	}
+})
+.parse(process.argv);
