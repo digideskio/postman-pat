184ae1c2cefe626f6ccd8d80c46bd481b293fda6
Tim Williams <tim.williams@baesystems.com>
2015-11-25 16:32:46 +0000

    Now working, fixed synchronicity

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var path = require('path');
 var program = require('commander');
 var fs = require('fs');
 var newman = require('newman');
+var queue = require('queue-async');
 
 program.arguments('<file> [otherFiles...]')
 .option('-e, --environment <environment>', 'The postman environment to use')
@@ -17,7 +18,7 @@ program.arguments('<file> [otherFiles...]')
 		otherFiles.forEach(function(otherFile) {
 			console.log(otherFile);
 		});
-		allFiles.concat(otherFiles);
+		allFiles = allFiles.concat(otherFiles);
 	}
 	
 	runPostmanForFiles(allFiles, program.dataPath, program.environment);
@@ -25,29 +26,54 @@ program.arguments('<file> [otherFiles...]')
 .parse(process.argv);
 
 function runPostmanForFiles(allFiles, dataPath, environment) {
+
+    // queue with a concurrency of 1 - i.e. run newman tasks sequentially
+	var q = queue(1);
+	var tasks = [];
 	allFiles.forEach(function(file) {
 		
 		var split = path.basename(file).split('.');
-		console.log('Looking for data for %s', split[0]);
+		var name = split[0];
+		console.log('Looking for data for %s', name);
 		
-		var dataFile = path.join(dataPath, split[0] + ".data.json");
-	    console.log('Checking %s', dataFile);
+		var dataFile = path.join(dataPath, name + ".data.json");
 		
 		if (!fs.statSync(dataFile).isFile()) {
-			console.log('File does not exist');
+			console.log('Data file %s does not exist', dataFile);
 			process.exit(1);
      	}
-		
-		runPostman(file, dataFile, environment);	
+		else {
+			console.log('Found data file %s', dataFile);
+		}
+
+	    tasks.push({
+				file: file,
+				dataFile: dataFile,
+				environment: environment
+			}
+		);		
+	})
+
+    console.log('Queueing %s newman task(s)...', tasks.length);
+	console.log('');
+	tasks.forEach(function(t) {
+		q.defer(runPostman, t.file, t.dataFile, t.environment);
+	})
+
+	// await all the tasks
+	q.awaitAll(function(error, results) {
+		console.log('');
+		console.log('Finished all tasks!');
+		process.exit();
 	})
 }
 
-function runPostman(file, dataFile, environment) {
+function runPostman(file, dataFile, environment, callback) {
 	
 	var newman = require('newman');
 	var JSON5 = require('json5');
 
-	// read the collectionjson file
+	// read the collectionJson file
 	var collectionJson = fs.readFileSync(file, 'utf8');
 	var collection = JSON5.parse(collectionJson);
 	
@@ -56,10 +82,16 @@ function runPostman(file, dataFile, environment) {
 		envJson: JSON5.parse(fs.readFileSync(environment, "utf-8")), // environment file (in parsed json format)
 		dataFile: dataFile,                    // data file if required
 		iterationCount: 1,                    // define the number of times the runner should run
-		outputFile: null,            // the file to export to
-		responseHandler: null, // the response handler to use
-		stopOnError: false
+		stopOnError: false,
+		responseHandler: "TestResponseHandler"
 	}
 	
-	newman.execute(collection, newmanOptions);
+	console.log('');
+	console.log('Running %s...', collection.name);
+	
+	newman.execute(collection, newmanOptions, function() {
+		callback();
+	});
 }
+
+
