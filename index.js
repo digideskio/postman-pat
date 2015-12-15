47c77ad66f4e151cff913e87b378874273a46f28
Tim Williams <tim.williams@baesystems.com>
2015-12-15 16:11:45 +0000

    Run newman via external command
    Since the newman js API doesn't work very well after repeat invocations
    due to all of the singletons, not to mention the memory leak, instead
    use a child_process to run newman as a command.
    
    Also tidied up the logging, colours and add a "-r" option for number of
    repetitions (not to be confused with Postman collection iterations).

--- index.js
+++ index.js
@@ -1,37 +1,42 @@
 #!/usr/bin/env node
-var newman = require('newman');
 var JSON5 = require('json5');
 var path = require('path');
 var program = require('commander');
 var fs = require('fs');
-var newman = require('newman');
 var queue = require('queue-async');
 var chalk = require('chalk');
+var newman = require('newman');
+var runCount = 1;
+var failCount = 0;
+var successCount = 0;
 
 program.arguments('<file> [otherFiles...]')
 .option('-e, --environment <environment>', 'The postman environment to use')
 .option('-d, --data-path <dataPath>', 'The directory to search for data files')
+.option('-r, --repetitions <repetitions>', 'The number of times to repeat (not the same as newman "iterations")')
 .action(function(file, otherFiles) {
 
 	var allFiles = [ file ];
 	if (otherFiles)	{
 		allFiles = allFiles.concat(otherFiles);
 	}
-	console.log(chalk.bold.gray.bgMagenta('                                              '));
-	console.log(chalk.bold.gray.bgMagenta(' Pat: the newman runner                       '));
-	console.log(chalk.bold.gray.bgMagenta('                                              '));
+
+    console.log('');	
+	console.log(chalk.magenta('Pat: the newman runner /////////'));
     console.log('');
+	
 	allFiles.forEach(function(file)	{
 		console.log(chalk.yellow('Collection: %s'), file);
 	})
+	
 	console.log('');
 	console.log(chalk.yellow.underline('Total: ' + allFiles.length + ' file(s)'));
 	console.log('');
 	console.log(chalk.cyan('Environment: %s'), program.environment);
 	console.log(chalk.blue('Data path: %s'), program.dataPath);
 	console.log('');
-	
-	runPostmanForFiles(allFiles, program.dataPath, program.environment);
+
+	runPostmanForFiles(allFiles, program.dataPath, program.environment);	
 })
 .parse(process.argv);
 
@@ -72,14 +77,23 @@ function runPostmanForFiles(allFiles, dataPath, environment) {
     console.log('Queueing %s newman task(s)...', tasks.length);
 
 	tasks.forEach(function(t) {
-		q.defer(runPostman, t.file, t.dataFile, t.environment);
+		q.defer(runPostmanExternal, t.file, t.dataFile, t.environment);
 	})
 
 	// await all the tasks
 	q.awaitAll(function(error, results) {
 		console.log('');
-		console.log('Finished all tasks!');
-		process.exit();
+		console.log('Finished all tasks for this run...');
+		if (runCount++ > program.repetitions) {
+			console.log(chalk.magenta('Finished all runs.'));
+			process.exit();
+		}
+		else {
+			console.log('');
+			console.log(chalk.magenta('Run #%s'), runCount);
+			console.log('');
+			runPostmanForFiles(allFiles, dataPath, environment);
+		}
 	})
 }
 
@@ -100,10 +114,56 @@ function runPostman(file, dataFile, environment, callback) {
 	
 	console.log('');
 	console.log('Running %s...', collection.name);
-	
+
 	newman.execute(collection, newmanOptions, function() {
 		callback();
 	});
 }
+
+function runPostmanExternal(file, dataFile, environment, callback) {
+	
+	var collectionJson = fs.readFileSync(file, 'utf8');
+	var collection = JSON5.parse(collectionJson);
+	
+	console.log('');
+	console.log('Running %s tests...', collection.name);
+	
+	var cmd = 'newman'
+	var args = [ '-c', file, 
+ 				 '-e', environment, 
+  				 '-d', dataFile,
+				 '-r', 40000, // timeout
+				 '-n', '1', // iterations
+				 '-j', // don't print summary
+				 '-x']; // use non-zero exit code to indicate one or more tests failed.
+
+	var spawn = require('child_process').spawn;
+    var newman = spawn(cmd, args);
+
+	newman.stdout.on('data', function (data) {
+		process.stdout.write(data);
+	});
+	
+	newman.stderr.on('data', function (data) {
+		process.stderr.write(data);
+	});
+	
+	newman.on('close', function (code) {
+		
+		if (code !== 0) {
+			console.log(chalk.red(''));
+			console.log(chalk.red('✗ One or more tests failed in this collection'));
+			failCount++;
+		}
+		else {
+			console.log(chalk.green(''));
+			console.log(chalk.green('✔ Clean sweep!'));
+			successCount++;
+		}
+		
+		callback();
+	});
+}
+
 
 
